ec0c97ef404991669a69cdcc5afe84f4b9f9f612
Nazar Vovk <nazarvovk.work@gmail.com>
2023-06-03 19:47:56 +0300

    chore: jest config update

--- jest.config.js
+++ jest.config.js
@@ -2,25 +2,15 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
   transform: {
-    ...tsjPreset.transform,
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.test.json',
+        isolatedModules: true, // Disable type checking for tests
+      },
+    ],
   },
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   roots: ['<rootDir>/src'],
-  globals: {
-    'ts-jest': {
-      /**
-       * Using a custom config for tests, which sets:
-       *
-       *   "include": []
-       *
-       * which makes tests run faster.
-       *
-       * Ref: https://github.com/kulshekhar/ts-jest/issues/259#issuecomment-617748411
-       */
-      tsconfig: 'tsconfig.test.json',
-      // Disable type checking for tests
-      isolatedModules: true,
-    },
-  },
 };
