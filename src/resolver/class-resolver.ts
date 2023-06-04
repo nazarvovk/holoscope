f9bcc6485d31c46f62278f673a7f9518183b4edc
Nazar Vovk <nazarvovk.work@gmail.com>
2023-06-04 20:43:38 +0300

    refactor: types

--- src/resolver/class-resolver.ts
+++ src/resolver/class-resolver.ts
@@ -2,7 +2,6 @@ import { Container } from '../types'
 import { FactoryResolver, FactoryResolverOptions } from './factory-resolver'
 
 type Class<TValue> = new (...args: any[]) => TValue
-type FunctionResolverOptions<TValue> = FactoryResolverOptions<TValue>
 
-export const asClass = <TValue>(class_: Class<TValue>, options?: FunctionResolverOptions<TValue>) =>
+export const asClass = <TValue>(class_: Class<TValue>, options?: FactoryResolverOptions<TValue>) =>
   new FactoryResolver((c: Container) => new class_(c), options)
--- src/resolver/function-resolver.ts
+++ src/resolver/function-resolver.ts
@@ -1,9 +1,8 @@
 import { FactoryResolver, FactoryResolverOptions } from './factory-resolver'
 
 type ConstructorFunction<TValue> = (...args: any[]) => TValue
-type FunctionResolverOptions<TValue> = FactoryResolverOptions<TValue>
 
 export const asFunction = <TValue>(
   function_: ConstructorFunction<TValue>,
-  options?: FunctionResolverOptions<TValue>,
+  options?: FactoryResolverOptions<TValue>,
 ) => new FactoryResolver(function_, options)
